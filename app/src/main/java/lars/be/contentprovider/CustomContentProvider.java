ac196def2f90604a978d43c6b49e9c00880e6da0
lars <larsvk@gmail.com>
2016-10-18 13:49:59 +0200

    Initial commit

---
+++ app/src/main/java/lars/be/contentprovider/CustomContentProvider.java
@@ -0,0 +1,188 @@
+package lars.be.contentprovider;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.util.HashMap;
+
+/**
+ * Created by brandonstark on 18/10/16.
+ */
+
+public class CustomContentProvider extends ContentProvider {
+
+    static final String PROVIDER_NAME = "com.androidatc.provider";
+    static final String URL = "content://"+ PROVIDER_NAME + "/nicknames";
+    static final Uri CONTENT_URI = Uri.parse(URL);
+
+    static final String ID = "id";
+    static final String NAME = "name";
+    static final String NICK_NAME = "nickname";
+
+    static final int NICKNAME = 1;
+    static final int NICKNAME_ID = 2;
+
+    DBHelper dbHelper;
+
+    private static HashMap<String, String> NicknameMap;
+    static final UriMatcher uriMatcher;
+    static{
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER_NAME, "nicknames", NICKNAME);
+        uriMatcher.addURI(PROVIDER_NAME, "nicknames/#", NICKNAME_ID);
+    }
+
+    private SQLiteDatabase database;
+    static final String DATABASE_NAME = "NicknamesDirectory";
+    static final String TABLE_NAME = "Nicknames";
+    static final int DATABASE_VERSION = 1;
+    static final String CREATE_TABLE =
+            "CREATE TABLE "
+                    + TABLE_NAME + " (id INTEGER PRIMARY KEY AUTOINCREMENT, " + " name TEXT NOT NULL, " + " nickname TEXT NOT NULL);";
+
+    private static class DBHelper extends SQLiteOpenHelper {
+        public DBHelper(Context context){
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(DBHelper.class.getName(),
+            "Upgrading database from version " + oldVersion + "to " + newVersion + ". Old data will be destroyed");
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
+        }
+    }
+
+    @Override
+    public boolean onCreate() {
+
+        Context context = getContext()
+;
+        dbHelper = new DBHelper(context);
+
+        database = dbHelper.getWritableDatabase()
+;
+        if (database == null){
+            return false;
+        } else {
+            return true;
+        }
+
+
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder){
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+
+        queryBuilder.setTables(TABLE_NAME);
+
+        switch(uriMatcher.match(uri)) {
+            case NICKNAME:
+                queryBuilder.setProjectionMap(NicknameMap);
+                break;
+            case NICKNAME_ID:
+                queryBuilder.appendWhere(ID + "=" + uri.getLastPathSegment());
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+
+
+
+        }
+        if (sortOrder == null || sortOrder ==""){
+            sortOrder = NAME;
+        }
+        Cursor cursor = queryBuilder.query(database, projection, selection, selectionArgs, null, null, sortOrder);
+        cursor.setNotificationUri(getContext().getContentResolver(),uri);
+        return cursor;
+
+    }
+
+    public Uri insert(Uri uri, ContentValues values){
+        long row = database.insert(TABLE_NAME,"",values);
+        if(row>0){
+            Uri newUri = ContentUris.withAppendedId(CONTENT_URI, row);
+            getContext().getContentResolver().notifyChange(newUri, null);
+            return newUri;
+        }
+
+    throw new SQLException("Fail to add a new record into " + uri);
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs){
+            int count = 0;
+
+        switch(uriMatcher.match(uri)){
+            case NICKNAME:
+                count = database.update(TABLE_NAME, values, selection, selectionArgs);
+                break;
+            case NICKNAME_ID:
+                count =  database.update(TABLE_NAME, values, ID + " = " + uri.getLastPathSegment() + (!TextUtils.isEmpty(selection) ? " AND (" + selection + ")" : ""), selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported URI " + uri);
+
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs){
+        int count = 0;
+
+        switch(uriMatcher.match(uri)){
+            case NICKNAME:
+                count = database.delete(TABLE_NAME, selection, selectionArgs);
+                break;
+            case NICKNAME_ID:
+                String id = uri.getLastPathSegment();
+                count =  database.delete(TABLE_NAME, ID + " = " + id + (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported URI " + uri);
+
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    @Override
+    public String getType(Uri uri){
+
+
+        switch(uriMatcher.match(uri)){
+            case NICKNAME:
+                return "vnd.android.cursor.dir/vnd.example.nicknames";
+
+            case NICKNAME_ID:
+                return "vnd.android.cursor.item/vnd.example.nicknames";
+
+            default:
+                throw new IllegalArgumentException("Unsupported URI " + uri);
+
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/lars/be/contentprovider/MainActivity.java
@@ -0,0 +1,159 @@
+package lars.be.contentprovider;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void addRecord(View view){
+        ContentValues values = new ContentValues();
+        if((!((EditText)findViewById(R.id.name)).getText().toString().isEmpty())
+                &&
+                (!((EditText)findViewById(R.id.nickname)).getText().toString().isEmpty())){
+            values.put(CustomContentProvider.NAME, ((EditText) findViewById(R.id.name)).getText().toString());
+            values.put(CustomContentProvider.NICK_NAME, ((EditText) findViewById(R.id.nickname)).getText().toString());
+
+            Uri uri = getContentResolver().insert(CustomContentProvider.CONTENT_URI, values);
+            Toast.makeText(getBaseContext(), "Record Inserted", Toast.LENGTH_LONG).show();
+
+
+        }
+        else{
+            Toast.makeText(getBaseContext(), "please enter the records first", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void showAllRecords(View view){
+        String URL = "content://com.androidatc.provider/nicknames";
+        Uri friends = Uri.parse(URL);
+        Cursor c = getContentResolver().query(friends, null, null, null, "name");
+        String result = "Content Provider Results:";
+
+        if (!c.moveToFirst()) {
+
+            Toast.makeText(this, result+" no content yet!", Toast.LENGTH_LONG).show();
+
+        }else{
+            do{
+                result = result + "\n"
+                        + c.getString(c.getColumnIndex(CustomContentProvider.NAME))
+                        + " has nickname "
+                        + c.getString(c.getColumnIndex(CustomContentProvider.NICK_NAME));
+            } while (c.moveToNext());
+            if(!result.isEmpty()){
+                Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "no records present", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    public void deleteAllRecords (View view){
+        String URL = "content://com.androidatc.provider/nicknames";
+        Uri friends = Uri.parse(URL);
+        int count = getContentResolver().delete(friends, null, null);
+        String countNum = count + " records are deleted.";
+        Toast.makeText(getBaseContext(), countNum, Toast.LENGTH_LONG).show();
+    }
+}
